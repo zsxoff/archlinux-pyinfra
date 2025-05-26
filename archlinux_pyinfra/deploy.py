fe0ffac44dadc64fb9a0765fde3ddec38b6b0ec6
Konstantin Dobratulin <zsxoff@gmail.com>
2025-05-26 23:02:40 +0300

    feat(deploy): update base deploy

--- archlinux_pyinfra/deploy.py
+++ archlinux_pyinfra/deploy.py
@@ -1,7 +1,4 @@
-from pyinfra import config
 from pyinfra.operations import pacman, server, systemd
-
-config.SUDO = True
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Locales
@@ -13,6 +10,7 @@ server.shell(
         'echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen',
         "locale-gen",
     ],
+    _sudo=True,
 )
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -20,8 +18,13 @@ server.shell(
 
 pacman.packages(
     name="Network - Install NetworkManager",
-    packages=["networkmanager", "wpa_supplicant"],
+    packages=[
+        "networkmanager",
+        "wireless-regdb",
+        "wpa_supplicant",
+    ],
     present=True,
+    _sudo=True,
 )
 
 systemd.service(
@@ -29,17 +32,13 @@ systemd.service(
     service="NetworkManager.service",
     running=True,
     enabled=True,
-)
-
-pacman.packages(
-    name="Network - Install wireless-regdb",
-    packages=["wireless-regdb"],
-    present=True,
+    _sudo=True,
 )
 
 server.shell(
     name="Network - Set regdomain to RU",
     commands=['echo "WIRELESS_REGDOM="RU"" > /etc/conf.d/wireless-regdom'],
+    _sudo=True,
 )
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -52,19 +51,16 @@ server.shell(
         "ln -s /usr/share/zoneinfo/Europe/Moscow /etc/localtime",
         "hwclock --systohc",
     ],
+    _sudo=True,
 )
 
 pacman.packages(
     name="Time - Install chrony",
-    packages=["chrony"],
+    packages=[
+        "chrony",
+    ],
     present=True,
-)
-
-systemd.service(
-    name="Time - Enable the chrony service",
-    service="chronyd.service",
-    running=True,
-    enabled=True,
+    _sudo=True,
 )
 
 systemd.service(
@@ -72,6 +68,15 @@ systemd.service(
     service="systemd-timesyncd.service",
     running=False,
     enabled=False,
+    _sudo=True,
+)
+
+systemd.service(
+    name="Time - Enable the chrony service",
+    service="chronyd.service",
+    running=True,
+    enabled=True,
+    _sudo=True,
 )
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -89,6 +94,7 @@ pacman.packages(
         "xfsprogs",
     ],
     present=True,
+    _sudo=True,
 )
 
 systemd.service(
@@ -96,6 +102,7 @@ systemd.service(
     service="fstrim.timer",
     running=True,
     enabled=True,
+    _sudo=True,
 )
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -105,24 +112,28 @@ pacman.packages(
     name="Firmware - Install Linux Firmware",
     packages=["linux-firmware"],
     present=True,
+    _sudo=True,
 )
 
 pacman.packages(
     name="Firmware - Install Intel microcode",
     packages=["intel-ucode"],
     present=True,
+    _sudo=True,
 )
 
 pacman.packages(
     name="Firmware - Install tool to manipulate Intel® IA-32/X86-64 microcode bundles",
     packages=["iucode-tool"],
     present=True,
+    _sudo=True,
 )
 
 server.modprobe(
     name="Firmware - Load the cpuid kernel module",
     module="cpuid",
     present=True,
+    _sudo=True,
 )
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -130,8 +141,15 @@ server.modprobe(
 
 pacman.packages(
     name="Audio - Install Pipewire",
-    packages=["pipewire", "pipewire-alsa", "pipewire-jack", "pipewire-pulse", "wireplumber"],
+    packages=[
+        "pipewire",
+        "pipewire-alsa",
+        "pipewire-jack",
+        "pipewire-pulse",
+        "wireplumber",
+    ],
     present=True,
+    _sudo=True,
 )
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -139,8 +157,12 @@ pacman.packages(
 
 pacman.packages(
     name="Bluetooth - Install bluez",
-    packages=["bluez", "bluez-libs", "bluez-utils", "blueman"],
+    packages=[
+        "blueman",
+        "bluez",
+    ],
     present=True,
+    _sudo=True,
 )
 
 systemd.service(
@@ -148,6 +170,7 @@ systemd.service(
     service="bluetooth.service",
     running=True,
     enabled=True,
+    _sudo=True,
 )
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -155,8 +178,11 @@ systemd.service(
 
 pacman.packages(
     name="Polkit - Install polkit",
-    packages=["polkit"],
+    packages=[
+        "polkit",
+    ],
     present=True,
+    _sudo=True,
 )
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -164,47 +190,11 @@ pacman.packages(
 
 pacman.packages(
     name="Python - Install Python",
-    packages=["python"],
+    packages=[
+        "python",
+    ],
     present=True,
-)
-
-pacman.packages(
-    name="Python - Install Python tools",
-    packages=["python-pip", "python-pipx", "python-setuptools"],
-    present=True,
-)
-
-# ----------------------------------------------------------------------------------------------------------------------
-# Go
-
-pacman.packages(
-    name="Go - Install Go",
-    packages=["go"],
-    present=True,
-)
-
-# ----------------------------------------------------------------------------------------------------------------------
-# NodeJS
-
-pacman.packages(
-    name="NodeJS - Install NodeJS",
-    packages=["nodejs"],
-    present=True,
-)
-
-pacman.packages(
-    name="NodeJS - Install npm",
-    packages=["npm"],
-    present=True,
-)
-
-# ----------------------------------------------------------------------------------------------------------------------
-# Java
-
-pacman.packages(
-    name="Java - Install OpenJDK",
-    packages=["jdk-openjdk"],
-    present=True,
+    _sudo=True,
 )
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -212,8 +202,11 @@ pacman.packages(
 
 pacman.packages(
     name="Fonts - Install fontconfig",
-    packages=["fontconfig"],
+    packages=[
+        "fontconfig",
+    ],
     present=True,
+    _sudo=True,
 )
 
 pacman.packages(
@@ -222,10 +215,10 @@ pacman.packages(
         "adobe-source-code-pro-fonts",
         "cantarell-fonts",
         "gnu-free-fonts",
+        "noto-fonts",
         "noto-fonts-cjk",
         "noto-fonts-emoji",
         "noto-fonts-extra",
-        "noto-fonts",
         "otf-font-awesome",
         "ttf-bitstream-vera",
         "ttf-croscore",
@@ -234,48 +227,79 @@ pacman.packages(
         "ttf-fira-code",
         "ttf-fira-mono",
         "ttf-fira-sans",
+        "ttf-hack",
         "ttf-ibm-plex",
-        "ttf-jetbrains-mono-nerd",
         "ttf-jetbrains-mono",
+        "ttf-jetbrains-mono-nerd",
         "ttf-liberation",
+        "ttf-nerd-fonts-symbols",
         "ttf-nerd-fonts-symbols-common",
         "ttf-nerd-fonts-symbols-mono",
-        "ttf-nerd-fonts-symbols",
         "ttf-opensans",
-        "ttf-roboto-mono",
         "ttf-roboto",
+        "ttf-roboto-mono",
         "ttf-ubuntu-font-family",
     ],
     present=True,
+    _sudo=True,
 )
 
-server.shell(name="Fonts - Generate fc-cache", commands=["fc-cache -f -v"])
+server.shell(
+    name="Fonts - Generate fc-cache",
+    commands=[
+        "fc-cache -f -v",
+    ],
+    _sudo=True,
+)
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Tools
 
 pacman.packages(
     name="Tools - Install pacman-contrib",
-    packages=["pacman-contrib"],
+    packages=[
+        "pacman-contrib",
+    ],
     present=True,
+    _sudo=True,
 )
 
 pacman.packages(
     name="Tools - Install base-devel",
-    packages=["base-devel"],
+    packages=[
+        "base-devel",
+    ],
     present=True,
+    _sudo=True,
 )
 
 pacman.packages(
     name="Tools - Install development packages",
-    packages=["cmake", "gcc", "make", "meson", "ninja"],
+    packages=[
+        "cmake",
+        "gcc",
+        "gdb",
+        "make",
+        "meson",
+        "ninja",
+    ],
     present=True,
+    _sudo=True,
 )
 
 pacman.packages(
     name="Tools - Install system tools",
-    packages=["file", "hwinfo", "hwloc", "upower", "usbmuxd", "usbutils"],
+    packages=[
+        "bolt",
+        "file",
+        "hwinfo",
+        "hwloc",
+        "upower",
+        "usbmuxd",
+        "usbutils",
+    ],
     present=True,
+    _sudo=True,
 )
 
 pacman.packages(
@@ -289,31 +313,47 @@ pacman.packages(
         "git",
         "highlight",
         "jq",
+        "mlocate",
         "nano",
+        "python-pygments",
         "ripgrep",
         "ripgrep-all",
-        "rsync",
         "starship",
         "wget",
         "yazi",
         "yq",
-        "zellij",
         "zoxide",
         "zsh",
     ],
     present=True,
+    _sudo=True,
 )
 
 pacman.packages(
     name="Tools - Install archive tools",
-    packages=["7zip", "atool", "tar", "unrar", "unzip", "xz", "zip", "zstd"],
+    packages=[
+        "7zip",
+        "atool",
+        "tar",
+        "unrar",
+        "unzip",
+        "xz",
+        "zip",
+        "zstd",
+    ],
     present=True,
+    _sudo=True,
 )
 
 pacman.packages(
     name="Tools - Install hunspell",
-    packages=["hunspell", "hunspell-en_us", "hunspell-ru"],
+    packages=[
+        "hunspell",
+        "hunspell-en_us",
+        "hunspell-ru",
+    ],
     present=True,
+    _sudo=True,
 )
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -323,24 +363,28 @@ pacman.packages(
     name="Performance - Install memtest86+-efi",
     packages=["memtest86+-efi"],
     present=True,
+    _sudo=True,
 )
 
 pacman.packages(
     name="Performance - Install systemtap",
     packages=["systemtap"],
     present=True,
+    _sudo=True,
 )
 
 pacman.packages(
     name="Performance - Install wireless_tools",
     packages=["wireless_tools"],
     present=True,
+    _sudo=True,
 )
 
 pacman.packages(
     name="Performance - Install cpupower",
     packages=["cpupower"],
     present=True,
+    _sudo=True,
 )
 
 systemd.service(
@@ -348,12 +392,14 @@ systemd.service(
     service="cpupower.service",
     running=True,
     enabled=True,
+    _sudo=True,
 )
 
 pacman.packages(
     name="Performance - Install thermald",
     packages=["thermald"],
     present=True,
+    _sudo=True,
 )
 
 systemd.service(
@@ -361,12 +407,14 @@ systemd.service(
     service="thermald.service",
     running=True,
     enabled=True,
+    _sudo=True,
 )
 
 pacman.packages(
     name="Performance - Install tuned-ppd",
     packages=["tuned-ppd"],
     present=True,
+    _sudo=True,
 )
 
 systemd.service(
@@ -374,6 +422,7 @@ systemd.service(
     service="tuned.service",
     running=True,
     enabled=True,
+    _sudo=True,
 )
 
 systemd.service(
@@ -381,19 +430,33 @@ systemd.service(
     service="tuned-ppd.service",
     running=True,
     enabled=True,
+    _sudo=True,
 )
 
 pacman.packages(
     name="Performance - Install fwupd",
-    packages=["fwupd", "dmidecode"],
+    packages=[
+        "dmidecode",
+        "fwupd",
+    ],
     present=True,
+    _sudo=True,
 )
 
+# systemd.service(
+#     name="Performance - Enable fwupd-refresh.timer",
+#     service="fwupd-refresh.timer",
+#     running=True,
+#     enabled=True,
+#     _sudo=True,
+# )
+
 systemd.service(
-    name="Performance - Enable fwupd-refresh.timer",
-    service="fwupd-refresh.timer",
+    name="Performance - Enable nvidia-powerd.service",
+    service="nvidia-powerd.service",
     running=True,
     enabled=True,
+    _sudo=True,
 )
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -407,10 +470,10 @@ pacman.packages(
         "gst-libav",
         "gst-plugin-pipewire",
         "gst-plugin-va",
-        "gst-plugins-bad-libs",
         "gst-plugins-bad",
-        "gst-plugins-base-libs",
+        "gst-plugins-bad-libs",
         "gst-plugins-base",
+        "gst-plugins-base-libs",
         "gst-plugins-good",
         "gst-plugins-ugly",
         "gstreamer",
@@ -419,118 +482,49 @@ pacman.packages(
         "x265",
     ],
     present=True,
+    _sudo=True,
 )
 
 pacman.packages(
     name="Desktop - Install GTK",
-    packages=["gtk3", "gtk4", "libappindicator-gtk3", "webkit2gtk"],
+    packages=[
+        "gtk3",
+        "gtk4",
+        "libappindicator-gtk2",
+        "libappindicator-gtk3",
+        "webkit2gtk",
+    ],
     present=True,
+    _sudo=True,
 )
 
 pacman.packages(
     name="Desktop - Install Qt",
-    packages=["qt5-base", "qt5-svg", "qt5-wayland", "qt6-base", "qt6-svg", "qt6-wayland"],
-    present=True,
-)
-
-pacman.packages(
-    name="Desktop - Install terminal",
-    packages=["kitty"],
-    present=True,
-)
-
-pacman.packages(
-    name="Desktop - Install Firefox",
-    packages=["firefox"],
-    present=True,
-)
-
-pacman.packages(
-    name="Desktop - Install MPV",
-    packages=["mpv"],
-    present=True,
-)
-
-pacman.packages(
-    name="Desktop - Install KeePassXC",
-    packages=["keepassxc"],
-    present=True,
-)
-
-pacman.packages(
-    name="Desktop - Install GDM",
-    packages=["gdm"],
-    present=True,
-)
-
-pacman.packages(
-    name="Desktop - Install Gnome",
     packages=[
-        "baobab",
-        "evince",
-        "eyedropper",
-        "file-roller",
-        "gnome-backgrounds",
-        "gnome-browser-connector",
-        "gnome-calendar",
-        "gnome-clocks",
-        "gnome-color-manager",
-        "gnome-contacts",
-        "gnome-control-center",
-        "gnome-disk-utility",
-        "gnome-firmware",
-        "gnome-font-viewer",
-        "gnome-keyring",
-        "gnome-logs",
-        "gnome-menus",
-        "gnome-music",
-        "gnome-session",
-        "gnome-settings-daemon",
-        "gnome-shell-extensions",
-        "gnome-shell",
-        "gnome-system-monitor",
-        "gnome-text-editor",
-        "gnome-tweaks",
-        "gnome-user-docs",
-        "gnome-weather",
-        "grilo-plugins",
-        "gvfs-afc",
-        "gvfs-dnssd",
-        "gvfs-gphoto2",
-        "gvfs-mtp",
-        "gvfs-nfs",
-        "gvfs-wsdd",
-        "gvfs",
-        "loupe",
-        "nautilus",
-        "papirus-icon-theme",
-        "python-pygments",
-        "rygel",
-        "snapshot",
-        "sushi",
-        "tecla",
-        "xdg-desktop-portal-gnome",
-        "xdg-user-dirs-gtk",
+        "qt5-base",
+        "qt5-imageformats",
+        "qt5-svg",
+        "qt5-wayland",
+        "qt6-base",
+        "qt6-imageformats",
+        "qt6-svg",
+        "qt6-wayland",
     ],
     present=True,
+    _sudo=True,
 )
 
 pacman.packages(
-    name="Desktop - Install wl-clipboard",
-    packages=["wl-clipboard"],
+    name="Desktop - Install Desktop tools",
+    packages=[
+        "firefox",
+        "foot",
+        "mpv",
+        "wl-clipboard",
+        "zed",
+    ],
     present=True,
-)
-
-systemd.service(
-    name="Desktop - Enable gdm.service",
-    service="gdm.service",
-    running=True,
-    enabled=True,
-)
-
-server.shell(
-    name="Desktop - Enable graphical.target",
-    commands=["systemctl set-default graphical.target"],
+    _sudo=True,
 )
 
 
@@ -538,8 +532,5 @@ server.shell(
 # sysctl
 
 server.sysctl(
-    name="sysctl - Enable tcp_window_scaling",
-    key="net.ipv4.tcp_window_scaling",
-    value=1,
-    persist=True,
+    name="sysctl - Enable tcp_window_scaling", key="net.ipv4.tcp_window_scaling", value=1, persist=True, _sudo=True
 )
