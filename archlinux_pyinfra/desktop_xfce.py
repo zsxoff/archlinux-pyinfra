f3a4aabd8b336fd1597c49cdc59e2a384b056d4f
Konstantin Dobratulin <zsxoff@gmail.com>
2025-12-02 00:13:46 +0300

    feat(desktop_xfce): add more hotkeys

--- archlinux_pyinfra/desktop_xfce.py
+++ archlinux_pyinfra/desktop_xfce.py
@@ -35,8 +35,10 @@ server.shell(
     #
     name="Keybinds - Add Xfce keybinds",
     commands=[
-        # Terminal
+        # Open Terminal
         f"xfconf-query -c xfce4-keyboard-shortcuts -p '/commands/custom/<Alt>Return' --create -v -t string -s ${TERMINAL}",
+        # Close window
+        "xfconf-query -c xfce4-keyboard-shortcuts -p '/xfwm4/custom/<Alt>Q' --create -v -t string -s close_window_key",
         # Change workspace
         "xfconf-query -c xfce4-keyboard-shortcuts -p '/xfwm4/custom/<Alt>1' --create -v -t string -s workspace_1_key",
         "xfconf-query -c xfce4-keyboard-shortcuts -p '/xfwm4/custom/<Alt>2' --create -v -t string -s workspace_2_key",
@@ -48,6 +50,17 @@ server.shell(
         "xfconf-query -c xfce4-keyboard-shortcuts -p '/xfwm4/custom/<Alt>8' --create -v -t string -s workspace_8_key",
         "xfconf-query -c xfce4-keyboard-shortcuts -p '/xfwm4/custom/<Alt>9' --create -v -t string -s workspace_9_key",
         "xfconf-query -c xfce4-keyboard-shortcuts -p '/xfwm4/custom/<Alt>0' --create -v -t string -s workspace_10_key",
+        # Move window to workspace
+        "xfconf-query -c xfce4-keyboard-shortcuts -p '/xfwm4/custom/<Alt><Shift>1' --create -v -t string -s move_window_workspace_1_key",
+        "xfconf-query -c xfce4-keyboard-shortcuts -p '/xfwm4/custom/<Alt><Shift>2' --create -v -t string -s move_window_workspace_2_key",
+        "xfconf-query -c xfce4-keyboard-shortcuts -p '/xfwm4/custom/<Alt><Shift>3' --create -v -t string -s move_window_workspace_3_key",
+        "xfconf-query -c xfce4-keyboard-shortcuts -p '/xfwm4/custom/<Alt><Shift>4' --create -v -t string -s move_window_workspace_4_key",
+        "xfconf-query -c xfce4-keyboard-shortcuts -p '/xfwm4/custom/<Alt><Shift>5' --create -v -t string -s move_window_workspace_5_key",
+        "xfconf-query -c xfce4-keyboard-shortcuts -p '/xfwm4/custom/<Alt><Shift>6' --create -v -t string -s move_window_workspace_6_key",
+        "xfconf-query -c xfce4-keyboard-shortcuts -p '/xfwm4/custom/<Alt><Shift>7' --create -v -t string -s move_window_workspace_7_key",
+        "xfconf-query -c xfce4-keyboard-shortcuts -p '/xfwm4/custom/<Alt><Shift>8' --create -v -t string -s move_window_workspace_8_key",
+        "xfconf-query -c xfce4-keyboard-shortcuts -p '/xfwm4/custom/<Alt><Shift>9' --create -v -t string -s move_window_workspace_9_key",
+        "xfconf-query -c xfce4-keyboard-shortcuts -p '/xfwm4/custom/<Alt><Shift>0' --create -v -t string -s move_window_workspace_10_key",
     ],
     _sudo=True,
 )
